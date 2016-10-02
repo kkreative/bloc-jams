995cec126f1901dbbdf51151716a36c54983bdd6
kkreative <kkreative@outlook.com>
2016-10-01 22:14:23 -0400

    Updating landing.js to reflect Assignment solution

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,12 +1,16 @@
-var revealPoint = function(point) {
-  point.style.opacity = 1;
-  point.style.transform = "scaleX(1) translateY(0)";
-  point.style.msTransform = "scaleX(1) translateY(0)";
-  point.style.WebkitTransform = "scaleX(1) translateY(0)";
+var animatePoints = function() {
+  var points = document.getElementsByClassName('point');
+
+  var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+  };
+
+
+
+  for (i = 0; i < points.length; i++) {
+    revealPoint(points[i]);
+  }
 };
-
-var points = document.getElementsByClassName('point');
-
-for (i = 0; i < points.length; i++) {
-  revealPoint(points[i]);
-}
