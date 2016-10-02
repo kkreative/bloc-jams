1cc6d934ef0c77a16dae6e34633100289cac3749
kkreative <kkreative@outlook.com>
2016-10-01 22:14:23 -0400

    Updating landing.js to reflect Assignment solution

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,12 +1,28 @@
-var revealPoint = function(point) {
-  point.style.opacity = 1;
-  point.style.transform = "scaleX(1) translateY(0)";
-  point.style.msTransform = "scaleX(1) translateY(0)";
-  point.style.WebkitTransform = "scaleX(1) translateY(0)";
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
+  var revealPoint = function(index) {
+    points[index].style.opacity = 1;
+    points[index].style.transform = "scaleX(1) translateY(0)";
+    points[index].style.msTransform = "scaleX(1) translateY(0)";
+    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+  };
+
+  for (i = 0; i < points.length; i++) {
+    revealPoint(i);
+  }
 };
 
-var points = document.getElementsByClassName('point');
+window.onload = function() {
+  if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
 
-for (i = 0; i < points.length; i++) {
-  revealPoint(points[i]);
-}
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  window.addEventListener('scroll', function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+                 animatePoints(pointsArray);
+             }
+    });
+ };
