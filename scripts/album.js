d7424319c3026f11d4b08b5925f18cebfffef6f9
kkreative <kkreative@outlook.com>
2016-11-13 10:23:48 -0500

    Checkpoint-19-NextPreviousJquery: adding fixtures.js

--- scripts/album.js
+++ scripts/album.js
@@ -1,52 +1,3 @@
-var albumPicasso = {
-    title: 'The Colors',
-    artist: 'Pablo Picasso',
-    label: 'Cubism',
-    year: '1881',
-    albumArtUrl: 'assets/images/album_covers/01.png',
-    songs: [{
-        title: 'Blue',
-        duration: '4:26'
-    }, {
-        title: 'Green',
-        duration: '3:14'
-    }, {
-        title: 'Red',
-        duration: '5:01'
-    }, {
-        title: 'Pink',
-        duration: '3:21'
-    }, {
-        title: 'Magenta',
-        duration: '2:15'
-    }]
-};
-
-// Another Example Album
-var albumMarconi = {
-    title: 'The Telephone',
-    artist: 'Guglielmo Marconi',
-    label: 'EM',
-    year: '1909',
-    albumArtUrl: 'assets/images/album_covers/20.png',
-    songs: [{
-        title: 'Hello, Operator?',
-        duration: '1:01'
-    }, {
-        title: 'Ring, ring, ring',
-        duration: '5:01'
-    }, {
-        title: 'Fits in your pocket',
-        duration: '3:21'
-    }, {
-        title: 'Can you hear me now?',
-        duration: '3:14'
-    }, {
-        title: 'Wrong phone number',
-        duration: '2:15'
-    }]
-};
-
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
         '<tr class="album-view-song-item">' +
@@ -101,6 +52,7 @@ var createSongRow = function(songNumber, songName, songLength) {
 
 var setCurrentAlbum = function(album) {
     // #1
+    currentAlbum = album;
     var $albumTitle = $('.album-view-title');
     var $albumArtist = $('.album-view-artist');
     var $albumReleaseInfo = $('.album-view-release-info');
@@ -125,7 +77,10 @@ var setCurrentAlbum = function(album) {
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
-var currentlyPlayingSong = null;
+
+var currentAlbum = null;
+var currentlyPlayingSongNumber = null;
+var currentSongFromAlbum = null;
 
 $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
